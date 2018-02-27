9d783b197cfcbff1547d545b93a6ccbbf25e0639
vitaozinho <vitor.macedo32@gmail.com>
2018-02-27 02:31:18 -0300

    First Experience

---
+++ Exemplo2/Exemplo2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Aula1_Exemplo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { //inicio main
+
+            //declarão de variaveis
+
+            int a, b, total;
+
+            //atribuição de valores
+
+            a = 20;
+            b = 10;
+
+            /*concatenação de valores a serem exibidos na tela
+             Calculo da operação sendo realiazado entre parenteses*/
+            Console.WriteLine(a + "+" + b + "=" + (a + b));
+
+            //Resultado do cálculo da operação sendo atribuido em uma variavel
+
+            total = a - b;
+            Console.WriteLine(a + "-" + b + "=" + total);
+            Console.WriteLine(a + "*" + b + "=" + (a * b));
+            Console.WriteLine(a + "/" + b + "=" + (a / b));
+            Console.WriteLine(a + "%" + b + "=" + (a % b));
+            Console.WriteLine("Hello Word");
+
+            Console.ReadKey();//instrução utilizada para dar uma pausa para visualizão 
+
+
+        }//fim main
+    }//fim class
+}//fim namespace
---
+++ Exemplo3/Exemplo3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //definição de constante pi
+        const double pi = 3.14159265358979323846264338327950;
+
+            //declaração de variavel e atribuição de valor na variavel
+            double raio = 10;
+            double area;
+
+            Console.WriteLine("Calculo da area de um circulo\n");
+            Console.WriteLine("Fórmula : area = pi * raio \n");
+
+            //Mat.pow: exponenciação
+            area = pi * Math.Pow(raio, 2); //sem usar o método pow seria: area = pi * area * area;
+
+            Console.WriteLine("Área = {0}", area); 
+            Console.WriteLine("Área = {0:N}", area);
+            Console.WriteLine("Área = {0:N5}", area);
+
+            Console.ReadKey();
+        }
+
+    }
+}
