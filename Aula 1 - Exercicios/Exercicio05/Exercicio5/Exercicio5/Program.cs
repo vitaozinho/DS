6c80865bc624d511e0325d91e8b89f042e7672c2
vitaozinho <vitor.macedo32@gmail.com>
2018-03-06 19:56:42 -0300

    Exercicio Aula 1 C#

---
+++ Aula 1 - Exercicios/Exercicio05/Exercicio5/Exercicio5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+            Console.Write("Digite sua idade: ");
+            idade = int.Parse(Console.ReadLine());
+
+            if (idade >= 18)
+                Console.WriteLine("\nVocê é maior de idade!");
+            else
+                Console.WriteLine("\nVocê é menor de idade!");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Aula 1 - Exercicios/Exercicio06/Exercicio6/Exercicio6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valreal, dolar, convert;
+            
+            Console.Write("Digite um valor em Reais(R$): ");
+            valreal = double.Parse(Console.ReadLine());
+
+            Console.Write("Digite a cotação do dólar(US$): ");
+            dolar = double.Parse(Console.ReadLine());
+
+            convert = valreal / dolar;
+
+            Console.WriteLine("\n{0} R$ em Dólares é: {1:N2} US$", valreal, convert);
+
+            Console.ReadKey();
+        }
+    }
+}
